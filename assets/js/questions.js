9092855cb5f6a47000928d799d1fb74d60e2caff
Louise Elliott <louise.elliott.work@gmail.com>
2023-11-03 10:04:10 +0000

    questions and answers structure added

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+
+var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+
+var questionOne = "Question 1 text here";
+var questionTwo = "Question 2 text here";
+var questionThree = "Question 3 text here";
+var questionFour = "Question 4 text here";
+var questionFive = "Question 5 text here";
+
+var answerOptions = [answerOptionsOne, answerOptionsTwo, answerOptionsThree, answerOptionsFour, answerOptionsFive];
+
+var answerOptionsOne = [q1optionA, q1optionB, q1optionC, q1optionD];
+var answerOptionsTwo = [q2optionA, q2optionB, q2optionC, q2optionD];
+var answerOptionsThree = [q3optionA, q3optionB, q3optionC, q3optionD];
+var answerOptionsFour = [q4optionA, q4optionB, q4optionC, q4optionD];
+var answerOptionsFive = [q5optionA, q5optionB, q5optionC, q5optionD];
+
+var correctAnswerOne = q1optionB;
+var correctAnswerOne = q2optionB;
+var correctAnswerOne = q3optionC;
+var correctAnswerOne = q4optionD;
+var correctAnswerOne = q5optionA;
