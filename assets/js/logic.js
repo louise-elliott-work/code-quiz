962438d9b81987a26f3980d1b88a73efab362112
Louise Elliott <louise.elliott.work@gmail.com>
2023-11-04 11:59:27 +0000

    clearInterval fixed and question section started

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,5 @@
+// * Task
+
 //A start button that when clicked a timer starts and the first question appears.
 //Questions contain buttons for each answer.
 //When answer is clicked, the next question appears.
@@ -6,29 +8,58 @@
 //When the game ends, it should display their score and give the user the ability to save their initials and their score.
 
 
+// * Timer
 
-//A start button that when clicked a timer starts and the first question appears.
-// Timer starts at 75 and reduces at one second intervals.
 var start = document.getElementById('start'); // Call start element in HTML code.
 var time = document.getElementById('time'); // Call timer element in HTML code.
-var timeRemaining = 75;
+var timerCount = 75;
+var timer;
 
-
-var countdown = setInterval(function () { 
-    time.textContent = timeRemaining;
-    timeRemaining--;
-    if(timeRemaining === 0) {
-    clearInterval(countdown);
-    // displayMessage(Message here to display score and give user ability to save initials and score. div ID end-screen);
-}
-}, 1000);
-
-function beginQuiz (event) {
-event.preventDefault;
-setInterval();
+function beginQuiz () {
+    console.log("Display first question and answer options");
+    startTimer()
+    displayQuestions()
 }
 
-start.addEventListener("click", beginQuiz);
+function startTimer () {
+    timer = setInterval (function() {
+        time.textContent = timerCount;
+        timerCount--;
+        if(timerCount < 0) {
+            clearInterval(timer);
+            // TODO #end-screen (message display score and give user ability to save initials and score)
+        }
+    }, 1000);
+}
 
-// TODO Correct code so countdown begins when start button is clicked, rather than when page is loaded.
+start.addEventListener('click', beginQuiz);
+
+
+// * Questions and answers
+
+var questionText = document.getElementById('question-title');
+var answerOptions = document.getElementById('options');
+var answerOption1 = document.getElementById('option1');
+var answerOption2 = document.getElementById('option2');
+var answerOption3 = document.getElementById('option3');
+var answerOption4 = document.getElementById('option4');
+
+function displayQuestions () {
+
+// TODO Work out how best to display questions and answer options using the mock-up provided.
+
+    // If statement here to check userAnswer against correctAnswer and trigger the next action accordingly.
+    if(!userAnswer) {
+        console.log("Waiting for user to answer");
+    }
+    else if (userAnswer === correctAnswer) {
+        console.log("Correct answer given - display congratulations message and play sound");
+    }
+    else {
+        console.log("Incorrect answer given - display explanation and motivational message or image");
+        // Add here reduction of time remaining.
+    }
+
+}
+
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,4 @@
+// Separate js file for questions to facilitate changes and additions.
 
 // Array of questions here to facilitate references and extension. Add text at the end.
 var questionsArray = [
@@ -10,20 +11,20 @@ var questionsArray = [
 
 // Array of answers here to facilitate references and extension. Add text at the end.
 var answerOptionsArray = [
-    [1, [A, "option A text here"], [B, "option B text here"], [C, "option C text here"], [D, "option D text here"]],
-    [2, [A, "option A text here"], [B, "option B text here"], [C, "option C text here"], [D, "option D text here"]],
-    [3, [A, "option A text here"], [B, "option B text here"], [C, "option C text here"], [D, "option D text here"]],
-    [4, [A, "option A text here"], [B, "option B text here"], [C, "option C text here"], [D, "option D text here"]],
-    [5, [A, "option A text here"], [B, "option B text here"], [C, "option C text here"], [D, "option D text here"]],
+    [1, ["A", "option A text here"], ["B", "option B text here"], ["C", "option C text here"], ["D", "option D text here"]],
+    [2, ["A", "option A text here"], ["B", "option B text here"], ["C", "option C text here"], ["D", "option D text here"]],
+    [3, ["A", "option A text here"], ["B", "option B text here"], ["C", "option C text here"], ["D", "option D text here"]],
+    [4, ["A", "option A text here"], ["B", "option B text here"], ["C", "option C text here"], ["D", "option D text here"]],
+    [5, ["A", "option A text here"], ["B", "option B text here"], ["C", "option C text here"], ["D", "option D text here"]],
 ]
 
 // Array of correct answers here reference in if statement below.
 var correctAnswersArray = [
-    [1, B],
-    [2, D],
-    [3, A],
-    [4, A],
-    [5, C],
+    [1, "B"],
+    [2, "D"],
+    [3, "A"],
+    [4, "A"],
+    [5, "C"],
 ];
 
 // userAnswer variable. This needs to store the value of the input by the user and it needs to change update for each new question.
@@ -33,12 +34,3 @@ var userAnswer;
 // correctAnswer variable. This needs to reference the correctAnswersArray to capture the correct answer for the specified question.
 // TODO Work out how best to reference this from the correctAnswersArray according to the question number.
 var correctAnswer;
-
-// If statement here to check userAnswer against correctAnswer and trigger the next action accordingly.
-if(userAnswer === correctAnswer) {
-    console.log("Correct answer given - display congratulations message and play sound");
-}
-else {
-    console.log("Incorrect answer given - display explanation and motivational message or image");
-    // Add here reduction of time remaining.
-}
