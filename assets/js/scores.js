ee5fa67f85eded45d730fc6922dfdd5fee1133ee
Louise Elliott <louise.elliott.work@gmail.com>
2023-11-09 17:16:52 +0000

    highscores basic pseudocoding done and submit redirection added

---
+++ assets/js/scores.js
@@ -0,0 +1,6 @@
+
+// On clicking submit...
+
+// store in an array of objects and add to local storage the user's initials (var userInitials) and the user's final score (var finalScore).
+
+// Add these two values to the ol #highscores, with the highest finalScore always showing at the top
